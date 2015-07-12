0aabd0bee62857ccbcf025a9a830915705427a3f
elran.k <elran.k@DELLMAR19-PC.taptica.local>
2015-07-12 18:20:47 +0300

    dsds

--- src/main/java/rtb/Starter.java
+++ src/main/java/rtb/Starter.java
@@ -7,8 +7,6 @@ public class Starter {
 		System.out.println("hello world");
 		System.out.println("elran 1st change");
 		System.out.println("elran 2nd change");
-		System.out.println("elran 3nd change and 4th change");
-		System.out.println("yogev first change");
 		
 	}
 }

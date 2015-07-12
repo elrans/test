6a0f11aabe901600dd0c06400fbdef39425612c5
elran.k <elran.k@DELLMAR19-PC.taptica.local>
2015-07-12 16:37:10 +0300

    push again

--- src/main/java/rtb/Starter.java
+++ src/main/java/rtb/Starter.java
@@ -5,5 +5,7 @@ public class Starter {
 	
 	public static void main(String [] args){
 		System.out.println("hello world");
+		System.out.println("elran 1st change");
+		System.out.println("elran 2nd change");
 	}
 }

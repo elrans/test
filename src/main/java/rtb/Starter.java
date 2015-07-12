23d1e8ca903640c1e5a3e013269cc1ce8424bd14
elran.k <elran.k@DELLMAR19-PC.taptica.local>
2015-07-12 16:32:27 +0300

    hello world

--- src/main/java/rtb/Starter.java
+++ src/main/java/rtb/Starter.java
@@ -5,7 +5,5 @@ public class Starter {
 	
 	public static void main(String [] args){
 		System.out.println("hello world");
-		System.out.println("elran 1st change");
-		System.out.println("elran 2nd change");
 	}
 }
